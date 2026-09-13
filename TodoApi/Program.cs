236e2a2ccf6fb34ce545e1f7f2e6e922dd44fecc
wanwisa seethapthim <wanwisa.see@ku.th>
2026-09-13 12:30:34 +0700

    [dev] add-delete-todo-endpoint

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,4 +1,3 @@
-using System.Linq.Expressions;
 using TodoApi.Dtos;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,33 +16,36 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+var todoGroup = app.MapGroup("/api/todos").WithTags("Todos");
+
 var todos = new List<TodoGetDto>
 {
-    new TodoGetDto(1,"Learn C#" , true),
-    new TodoGetDto(2,"Learn ASP.NET Core" , false),
-    new TodoGetDto(3,"Build a web API" , false)
-};
+    new TodoGetDto(1, "Learn C#", true),
+    new TodoGetDto(2, "Learn ASP.NET Core", false),
+    new TodoGetDto(3, "Build a web API", false)
+};  
 
-app.MapGet("/api/todos",() => Results.Ok(todos));
+todoGroup.MapGet("/", () => Results.Ok(todos));
 
-app.MapPost("/api/todos", (TodoPostDto dto) =>
-{
-    var nextId = todos.Count == 0 ? 1 : todos.Max(t => t.Id) + 1;
-
-    var todo = new TodoGetDto(nextId, dto.Title, false);
-    todos.Add(todo);
-    return Results.Created($"/api/todos/{todo.Id}", todo);
-});
-
-app.MapGet("/api/todos/{id}", (int id) =>
+todoGroup.MapGet("/{id}", (int id) =>
 {
     var todo = todos.FirstOrDefault(t => t.Id == id);
-    return todo is null
-        ? Results.NotFound()
-        : Results.Ok(todo);
-});
 
-app.MapPut("/api/todos/{id}", (int id, TodoPutDto dto) =>
+    return todo is not null ? Results.Ok(todo) : Results.NotFound();
+        
+});
+    
+    todoGroup.MapPost("/", (TodoPostDto dto) =>
+    {
+        var nextId = todos.Count == 0 ? 1 : todos.Max(t => t.Id) + 1;
+
+        var todo = new TodoGetDto(nextId, dto.Title, false);
+        todos.Add(todo);
+
+        return Results.Created($"/api/todos/{todo.Id}", todo);
+    });
+
+todoGroup.MapPut("/{id}", (int id, TodoPutDto dto) =>
 {
     try
     {
@@ -53,7 +55,7 @@ app.MapPut("/api/todos/{id}", (int id, TodoPutDto dto) =>
         todos[index] = todos[index] with
         {
             Title = dto.Title,
-            IsCompleted = dto.Iscompleted
+            IsCompleted = dto.IsCompleted 
         };
 
         return Results.Ok(todos[index]);
@@ -62,6 +64,27 @@ app.MapPut("/api/todos/{id}", (int id, TodoPutDto dto) =>
     {
         return Results.Problem(ex.Message);
     }
+});
+
+todoGroup.MapDelete("/{id}", (int id) =>
+{
+    try
+    {
+        var todo = todos.FirstOrDefault(t => t.Id == id);
+        if (todo is null) return Results.NotFound();
+
+        todos.Remove(todo);
+        return Results.NoContent();
+    }
+    catch(ArgumentNullException ex)
+    {
+        return Results.Problem($"Parameter is null: {ex.ParamName}");
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+
 });
 
 app.Run();
